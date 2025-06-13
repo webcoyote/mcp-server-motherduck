92c03eb68d5de9b55e8f0c9dcf1b968eef5ecee6
Adithya Krishnan <krishsandeep@gmail.com>
2025-06-13 17:38:06 +0200

    run sse

--- src/mcp_server_motherduck/__init__.py
+++ src/mcp_server_motherduck/__init__.py
@@ -1,66 +1,118 @@
-from . import server
-import asyncio
-import argparse
+import anyio
 import logging
+import click
+from .server import build_application
+from .configs import SERVER_VERSION
 
 logger = logging.getLogger("mcp_server_motherduck")
 logging.basicConfig(level=logging.INFO, format="[%(name)s] %(levelname)s - %(message)s")
 
 
-def main():
+@click.command()
+@click.option("--port", default=8000, help="Port to listen on for SSE")
+@click.option(
+    "--transport",
+    type=click.Choice(["stdio", "sse"]),
+    default="stdio",
+    help="(Default: `stdio`) Transport type",
+)
+@click.option(
+    "--db-path",
+    default="md:",
+    help="(Default: `md:`) Path to local DuckDB database file or MotherDuck database",
+)
+@click.option(
+    "--motherduck-token",
+    default=None,
+    help="(Default: env var `motherduck_token`) Access token to use for MotherDuck database connections",
+)
+@click.option(
+    "--home-dir",
+    default=None,
+    help="(Default: env var `HOME`) Home directory for DuckDB",
+)
+@click.option(
+    "--saas-mode",
+    is_flag=True,
+    help="Flag for connecting to MotherDuck in SaaS mode",
+)
+@click.option(
+    "--result-format",
+    default="markdown",
+    type=click.Choice(["markdown", "duckbox", "text"]),
+    help="(Default: `markdown`) Format of the query result",
+)
+@click.option(
+    "--read-only",
+    is_flag=True,
+    help="Flag for connecting to DuckDB in read-only mode. Only supported for local DuckDB databases. Also makes use of short lived connections so multiple MCP clients or other systems can remain active (though each operation must be done sequentially).",
+)
+def main(
+    port,
+    transport,
+    db_path,
+    motherduck_token,
+    home_dir,
+    saas_mode,
+    result_format,
+    read_only,
+):
     """Main entry point for the package."""
 
-    parser = argparse.ArgumentParser(description="MotherDuck MCP Server")
-    parser.add_argument(
-        "--db-path",
-        default="md:",
-        help="(Default: `md:`) Path to local DuckDB database file or MotherDuck database",
-    )
-    parser.add_argument(
-        "--motherduck-token",
-        default=None,
-        help="(Default: env var `motherduck_token`) Access token to use for MotherDuck database connections",
-    )
-    parser.add_argument(
-        "--home-dir",
-        default=None,
-        help="(Default: env var `HOME`) Home directory for DuckDB",
-    )
-    parser.add_argument(
-        "--saas-mode",
-        action="store_true",
-        help="Flag for connecting to MotherDuck in SaaS mode",
-    )
-    # This is experimental and will change in the future
-    parser.add_argument(
-        "--result-format",
-        help="(Default: `markdown`) Format of the query result",
-        default="markdown",
-        choices=["markdown", "duckbox", "text"],
-    )
-
-    parser.add_argument(
-        "--read-only",
-        action="store_true",
-        help="Flag for connecting to DuckDB in read-only mode. Only supported for local DuckDB databases. Also makes use of short lived connections so multiple MCP clients or other systems can remain active (though each operation must be done sequentially).",
-    )
-
-    args = parser.parse_args()
-    logger.info("🦆 MotherDuck MCP Server v" + server.SERVER_VERSION)
+    logger.info("🦆 MotherDuck MCP Server v" + SERVER_VERSION)
     logger.info("Ready to execute SQL queries via DuckDB/MotherDuck")
-    logger.info("Waiting for client connection...\n")
+    logger.info("Waiting for client connection...")
 
-    asyncio.run(
-        server.main(
-            db_path=args.db_path,
-            motherduck_token=args.motherduck_token,
-            result_format=args.result_format,
-            home_dir=args.home_dir,
-            saas_mode=args.saas_mode,
-            read_only=args.read_only,
-        )
+    app, init_opts = build_application(
+        db_path=db_path,
+        motherduck_token=motherduck_token,
+        result_format=result_format,
+        home_dir=home_dir,
+        saas_mode=saas_mode,
+        read_only=read_only,
     )
+
+    if transport == "sse":
+        from mcp.server.sse import SseServerTransport
+        from starlette.applications import Starlette
+        from starlette.responses import Response
+        from starlette.routing import Mount, Route
+
+        sse = SseServerTransport("/messages/")
+
+        async def handle_sse(request):
+            async with sse.connect_sse(
+                request.scope, request.receive, request._send
+            ) as (read_stream, write_stream):
+                await app.run(read_stream, write_stream, init_opts)
+            return Response()
+
+        starlette_app = Starlette(
+            debug=True,
+            routes=[
+                Route("/sse", endpoint=handle_sse, methods=["GET"]),
+                Mount("/messages/", app=sse.handle_post_message),
+            ],
+        )
+
+        import uvicorn
+
+        uvicorn.run(starlette_app, host="127.0.0.1", port=port)
+    else:
+        from mcp.server.stdio import stdio_server
+
+        async def arun():
+            async with stdio_server() as (read_stream, write_stream):
+                await app.run(read_stream, write_stream, init_opts)
+
+        anyio.run(arun)
+
+    # This will only be reached when the server is shutting down
+    logger.info("\n🦆 MotherDuck MCP Server shutting down...")
 
 
 # Optionally expose other important items at package level
-__all__ = ["main", "server"]
+__all__ = ["main"]
+
+if __name__ == "__main__":
+    main()
---
+++ src/mcp_server_motherduck/database.py
@@ -0,0 +1,150 @@
+import os
+import duckdb
+from typing import Literal, Optional
+import io
+from contextlib import redirect_stdout
+from tabulate import tabulate
+import logging
+from .configs import SERVER_VERSION
+
+logger = logging.getLogger("mcp_server_motherduck")
+
+
+class DatabaseClient:
+    def __init__(
+        self,
+        db_path: str | None = None,
+        motherduck_token: str | None = None,
+        result_format: Literal["markdown", "duckbox", "text"] = "markdown",
+        home_dir: str | None = None,
+        saas_mode: bool = False,
+        read_only: bool = False,
+    ):
+        self._read_only = read_only
+        self.db_path, self.db_type = self._resolve_db_path_type(
+            db_path, motherduck_token, saas_mode
+        )
+        logger.info(f"Database client initialized in `{self.db_type}` mode")
+
+        # Set the home directory for DuckDB
+        if home_dir:
+            os.environ["HOME"] = home_dir
+
+        self.conn = self._initialize_connection()
+        self.result_format = result_format
+
+    def _initialize_connection(self) -> Optional[duckdb.DuckDBPyConnection]:
+        """Initialize connection to the MotherDuck or DuckDB database"""
+
+        logger.info(f"🔌 Connecting to {self.db_type} database")
+
+        if self.db_type == "duckdb" and self._read_only:
+            # check that we can connect, issue a `select 1` and then close + return None
+            try:
+                conn = duckdb.connect(
+                    self.db_path,
+                    config={
+                        "custom_user_agent": f"mcp-server-motherduck/{SERVER_VERSION}"
+                    },
+                    read_only=self._read_only,
+                )
+                conn.execute("SELECT 1")
+                conn.close()
+                return None
+            except Exception as e:
+                logger.error(f"❌ Read-only check failed: {e}")
+                raise
+
+        if self._read_only:
+            raise ValueError(
+                "Read-only mode is only supported for local DuckDB databases. See `saas_mode` for similar functionality with MotherDuck."
+            )
+        conn = duckdb.connect(
+            self.db_path,
+            config={"custom_user_agent": f"mcp-server-motherduck/{SERVER_VERSION}"},
+        )
+
+        logger.info(f"✅ Successfully connected to {self.db_type} database")
+
+        return conn
+
+    def _resolve_db_path_type(
+        self, db_path: str, motherduck_token: str | None = None, saas_mode: bool = False
+    ) -> tuple[str, Literal["duckdb", "motherduck"]]:
+        """Resolve and validate the database path"""
+        # Handle MotherDuck paths
+        if db_path.startswith("md:"):
+            if motherduck_token:
+                logger.info("Using MotherDuck token to connect to database `md:`")
+                if saas_mode:
+                    logger.info("Connecting to MotherDuck in SaaS mode")
+                    return (
+                        f"{db_path}?motherduck_token={motherduck_token}&saas_mode=true",
+                        "motherduck",
+                    )
+                else:
+                    return (
+                        f"{db_path}?motherduck_token={motherduck_token}",
+                        "motherduck",
+                    )
+            elif os.getenv("motherduck_token"):
+                logger.info(
+                    "Using MotherDuck token from env to connect to database `md:`"
+                )
+                return (
+                    f"{db_path}?motherduck_token={os.getenv('motherduck_token')}",
+                    "motherduck",
+                )
+            else:
+                raise ValueError(
+                    "Please set the `motherduck_token` as an environment variable or pass it as an argument with `--motherduck-token` when using `md:` as db_path."
+                )
+
+        if db_path == ":memory:":
+            return db_path, "duckdb"
+
+        # Handle local database paths as the last check
+        if not os.path.exists(db_path):
+            raise FileNotFoundError(
+                f"The local database path `{db_path}` does not exist."
+            )
+        return db_path, "duckdb"
+
+    def _execute(self, query: str) -> str:
+        if self.conn is None:
+            # open short lived readonly connection, run query, close connection, return result
+            conn = duckdb.connect(
+                self.db_path,
+                config={"custom_user_agent": f"mcp-server-motherduck/{SERVER_VERSION}"},
+                read_only=self._read_only,
+            )
+            q = conn.execute(query)
+        else:
+            q = self.conn.execute(query)
+
+        if self.result_format == "markdown":
+            out = tabulate(
+                q.fetchall(),
+                headers=[d[0] + "\n" + d[1] for d in q.description],
+                tablefmt="pretty",
+            )
+        elif self.result_format == "duckbox":
+            # Duckbox version of the output
+            buffer = io.StringIO()
+            with redirect_stdout(buffer):
+                q.show(max_rows=100, max_col_width=20)
+            out = buffer.getvalue()
+        else:
+            out = str(q.fetchall())
+
+        if self.conn is None:
+            conn.close()
+
+        return out
+
+    def query(self, query: str) -> str:
+        try:
+            return self._execute(query)
+
+        except Exception as e:
+            raise ValueError(f"❌ Error executing query: {e}")
